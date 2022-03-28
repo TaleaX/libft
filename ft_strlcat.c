42066a1563b9dbe2221b45954b08430f5eb62e40
TaleaX <sophie.dehne01@gmail.com>
2022-03-28 19:48:12 +0200

    on its way

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: tdehne <tdehne@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/26 15:24:35 by tdehne            #+#    #+#             */
-/*   Updated: 2022/03/26 15:47:11 by tdehne           ###   ########.fr       */
+/*   Updated: 2022/03/28 19:47:31 by tdehne           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,22 +27,25 @@ static int	ft_str(char *str)
 size_t	ft_strlcat(char *dest, const char *src, size_t size)
 {
 	size_t	i;
+	size_t	j;
 	int		len_dest;
 
 	i = 0;
-	if (size < 0)
+	j = 0;
+	if (size > 0)
 	{
 		len_dest = ft_str(dest);
-		while (*dest)
-			dest++;
-		while (i < (size - len_dest - 1) && src[i])
-		{
-			dest[i] = src[i];
+		while (dest[i])
 			i++;
+		while (i < (size - len_dest - 1) && src[j])
+		{
+			dest[i] = src[j];
+			i++;
+			j++;
 		}
 		dest[i] = '\0';
 	}
-	while (src[i])
-		i++;
+	while (src[j])
+		j++;
 	return (i);
 }
