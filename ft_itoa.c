24cadebafec586191280682a9c806e9257cfbf0d
TaleaX <sophie.dehne01@gmail.com>
2022-04-01 15:41:50 +0200

    works

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,20 +6,21 @@
 /*   By: tdehne <tdehne@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/26 16:08:25 by tdehne            #+#    #+#             */
-/*   Updated: 2022/03/28 18:41:21 by tdehne           ###   ########.fr       */
+/*   Updated: 2022/04/01 15:41:10 by tdehne           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
 
-static int	get_strnum_len(int n)
+static int	get_strnum_len(long n)
 {
 	int	counter;
 
 	counter = 0;
 	if (!n)
 		return (1);
+	if (n < 0)
+		n *= -1;
 	while (n > 0)
 	{
 		counter++;
@@ -28,21 +29,26 @@ static int	get_strnum_len(int n)
 	return (counter);
 }
 
+static long	ft_abs(long n)
+{
+	if (n < 0)
+		return (n * -1);
+	return (n);
+}
+
 char	*ft_itoa(int n)
 {
-	char	*str;
-	int		remainder;
-	int		i;
-	int		len;
-	int		minus;
+	char		*str;
+	int			i;
+	int			len;
+	int			minus;
+	long		num;
 
 	minus = 0;
 	if (n < 0)
-	{
 		minus = 1;
-		n *= -1;
-	}
-	len = get_strnum_len(n);
+	num = ft_abs((long) n);
+	len = get_strnum_len(num);
 	if (minus)
 		len++;
 	str = (char *) malloc(sizeof(char) * len + 1);
@@ -50,9 +56,8 @@ char	*ft_itoa(int n)
 	str[len] = '\0';
 	while (i >= 0)
 	{
-		remainder = n % 10;
-		n = (n - remainder) / 10;
-		str[i] = remainder + '0';
+		str[i] = (num % 10) + '0';
+		num = num / 10;
 		if (minus && i == 0)
 			str[i] = '-';
 		i--;
